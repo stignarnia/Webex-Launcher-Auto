eb10a3b05517007a7a7cc20cf04ea0e3d48834ba
stignarnia <80171209+stignarnia@users.noreply.github.com>
2021-05-21 18:33:44 +0200

    Update WAL_puppeteer.cs

--- WAL_puppeteer.cs
+++ WAL_puppeteer.cs
@@ -1,7 +1,8 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Threading.Tasks;
 using PuppeteerSharp;
+using Webex_Launcher_Auto;
 
 namespace WAL_puppeteer
 {
@@ -10,23 +11,9 @@ namespace WAL_puppeteer
         public static async Task DownloadSchedule()
         {
             int i;
-            string downloadPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+            string path = Program.IsBrowserInstalled();
 
-            var browserFetcherOptions = new BrowserFetcherOptions { Path = downloadPath };
-            var browserFetcher = new BrowserFetcher(browserFetcherOptions);
-            await browserFetcher.DownloadAsync();
-
-            var list = Directory.GetDirectories(downloadPath);
-            foreach (string isThisChrome in list)
-            {
-                if (isThisChrome.Contains("Win64-"))
-                {
-                    downloadPath = isThisChrome + @"\chrome-win\chrome.exe";
-                    break;
-                }
-            }
-
-            var browser = await Puppeteer.LaunchAsync(new LaunchOptions { Headless = false, ExecutablePath = downloadPath });
+            var browser = await Puppeteer.LaunchAsync(new LaunchOptions { Headless = false, ExecutablePath = path });
             Page page = await browser.NewPageAsync();
             await page.GoToAsync("https://aunicalogin.polimi.it/aunicalogin/aunicalogin.jsp?id_servizio=376&profile=0&lang=IT&polij_device_category=DESKTOP&__pj0=0&__pj1=b015f2f9bdbbc23774f309454d1cb027");
             for (i = 0; i < 4; i++)
